9dc405df74b47fd2970c3f07a35d4c524a0c9ec3
SheykoWk <sheyko.wk@gmail.com>
2022-03-22 22:21:20 -0600

    my first commit

---
+++ src/components/MyFirstComponent.js
@@ -0,0 +1,12 @@
+import "./MyFirstComponent.css";
+
+const MyFirstComponent = () => {
+  return (
+    <div class="green">
+      <button>Hola</button>
+      <div>Mundo</div>
+    </div>
+  );
+};
+
+export default MyFirstComponent;
